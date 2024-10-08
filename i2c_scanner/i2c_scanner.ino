25bd2e06e62e9cf62991748e1e66b2445010ce28
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-10-08 23:36:14 +0300

    move to setup

--- i2c_scanner/i2c_scanner.ino
+++ i2c_scanner/i2c_scanner.ino
@@ -12,58 +12,47 @@ void setup() {
   while (!Serial);
   Serial.println("\nI2C Scanner");
 
-}
- 
-void loop() {
-  Serial.println("Scanning...");
- 
+  // check devices
   // LCD screen - 0x27
   byte lcd_address = 39;
   Wire.beginTransmission(lcd_address);
   byte error_lcd = Wire.endTransmission();
   if (error_lcd == 0) {
-    Serial.print("LCD device found at address 0x");
-    Serial.println(lcd_address, HEX);
     lcd_found = 1;
   } else {
     lcd_found = 0;
   }
-
   // Fenyero mero - 0x39
   byte light_address = 57;
   Wire.beginTransmission(light_address);
   byte error_ligt = Wire.endTransmission();
   if (error_ligt == 0) {
-    Serial.print("Light device found at address 0x");
-    Serial.println(light_address, HEX);
     light_found = 1;
   } else {
     light_found = 0;
   }
-
   // RTC clock mero - 0x68
   byte rtc_address = 104;
   Wire.beginTransmission(rtc_address);
   byte error_clock = Wire.endTransmission();
   if (error_clock == 0) {
-    Serial.print("RTC clock found at address 0x");
-    Serial.println(rtc_address, HEX);
     rtc_clock_found = 1;
   } else {
     rtc_clock_found = 0;
   }
 
-  if (lcd_found == 0) { 
-    Serial.println("LCD device not found");
+  if (lcd_found == 1) { 
+    Serial.println("LCD device found");
   } 
-  if (light_found == 0) {
-    Serial.println("Light device not found");
+  if (light_found == 1) {
+    Serial.println("Light device found");
   } 
-  if (rtc_clock_found == 0) {
-    Serial.println("RTC clock not found");
+  if (rtc_clock_found == 1) {
+    Serial.println("RTC clock found");
   } 
 
-  Serial.println("done\n");
-  delay(1000);
-
+}
+ 
+void loop() {
+  delay(5000);
 }
