426d5a592ce83f6b36e418c733befc7d26acac23
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-10-12 14:56:18 +0300

    merge code

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal_I2C.h>
 #include "DHT.h"
+#include <Wire.h>
 
 #define DHTPIN A3
 #define DHTTYPE DHT11
@@ -12,6 +13,10 @@ int gomb_allapota;
 int gomb_regi_allapota;
 
 int dht_fail;
+
+int lcd_found;
+int light_found;
+int rtc_clock_found;
 
 void setup() {
   pinMode(A0,INPUT_PULLUP);
@@ -35,6 +40,9 @@ void setup() {
 
   lcd.setCursor(0,1);
   lcd.print("Temp: ");
+
+  Wire.begin();
+  checkI2cDevices();
 }
 
 void loop() {
@@ -77,4 +85,47 @@ void loop() {
   }
 
   delay(250);
+}
+
+
+void checkI2cDevices() {
+  // check devices
+  // LCD screen - 0x27
+  byte lcd_address = 39;
+  Wire.beginTransmission(lcd_address);
+  byte error_lcd = Wire.endTransmission();
+  if (error_lcd == 0) {
+    lcd_found = 1;
+  } else {
+    lcd_found = 0;
+  }
+  // Fenyero mero - 0x39
+  byte light_address = 57;
+  Wire.beginTransmission(light_address);
+  byte error_ligt = Wire.endTransmission();
+  if (error_ligt == 0) {
+    light_found = 1;
+  } else {
+    light_found = 0;
+  }
+  // RTC clock mero - 0x68
+  byte rtc_address = 104;
+  Wire.beginTransmission(rtc_address);
+  byte error_clock = Wire.endTransmission();
+  if (error_clock == 0) {
+    rtc_clock_found = 1;
+  } else {
+    rtc_clock_found = 0;
+  }
+
+  if (lcd_found == 1) { 
+    Serial.println("LCD device found");
+  } 
+  if (light_found == 1) {
+    Serial.println("Light device found");
+  } 
+  if (rtc_clock_found == 1) {
+    Serial.println("RTC clock found");
+  } 
+  Serial.println();
 }
