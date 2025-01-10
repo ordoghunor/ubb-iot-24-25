926cb54da5d40d61094e56b8cb2d58c807965642
Pap Norbert <116946410+PapNorbert@users.noreply.github.com>
2025-01-10 15:53:02 +0200

    old data request

--- project/node/node.ino
+++ project/node/node.ino
@@ -5,6 +5,8 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
+#include <map>
+
 SPISettings spi_settings(100000, MSBFIRST, SPI_MODE0); 
 // 100 kHz
 
@@ -34,8 +36,8 @@ const long intervalFirebase = 5000;
 
 String currentDate = "";
 
-const char* ssid = "TP-Link_871C";
-const char* password = "almakorte";
+const char* ssid = "Mateinfo";
+const char* password = "computer";
 
 #define API_KEY "AIzaSyBc0-NTp3oeJFd4o9l1SBQvvDRv8XsJwCk"
 #define DATABASE_URL "https://iot-hln-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -46,6 +48,10 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 AsyncWebServer server(80);
+
+std::map<String, String> olderDataDictionary;
+String timeParamArray[10];
+int timeParamCount = 0;
 
 
 void setup() {
@@ -66,7 +72,7 @@ void setup() {
 
   // Serve the main HTML page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/html", generateHtmlPage());
+    request->send(200, "text/html; charset=UTF-8", generateHtmlPage());
   });
 
   server.on("/getData", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -96,16 +102,28 @@ void setup() {
     }
   });
 
+  server.on("/requestOlderData", HTTP_GET, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("time")) {
+      String timeParam = request->getParam("time")->value();
+      if (timeParamCount < 10) {
+        timeParamArray[timeParamCount++] = timeParam;
+      }
+      request->send(200, "text/plain", "Ok");
+    } else {
+      request->send(400, "text/plain", "Missing time parameter.");
+    }
+  });
+
   server.on("/getOlderData", HTTP_GET, [](AsyncWebServerRequest *request) {
     if (request->hasParam("time")) {
       String timeParam = request->getParam("time")->value();
-      Serial.println(timeParam);
-      String data = "{}";
-      request->send(200, "application/json", "{\"data\": \"" + data + "\"}");
+      String data = olderDataDictionary[timeParam];
+      olderDataDictionary.erase(timeParam);
+      request->send(200, "application/json", data);
     } else {
         request->send(400, "text/plain", "Missing time parameter.");
     }
-});
+  });
 
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
@@ -132,7 +150,7 @@ void loop() {
     previousMillisFirebase = millis();
     saveDataToFirebase();
   }
-  
+  processTimeParamArray();
   delay(500);
 }
 
@@ -160,11 +178,11 @@ void updateSensorData() {
     buttonString = String(button, DEC);
     motorRunningString = String(motorRunning);
 
-    if (currentDate == "") {
-      currentDate = "20" + String(year, DEC) + "-";
-      currentDate += (month < 10 ? "0" : "") + String(month, DEC) + "-";
-      currentDate += (day < 10 ? "0" : "") + String(day, DEC);
-    }
+    currentDate = "20" + String(year, DEC) + "-";
+    currentDate += (month < 10 ? "0" : "") + String(month, DEC) + "-";
+    currentDate += (day < 10 ? "0" : "") + String(day, DEC)+ "-";
+    currentDate += (hour < 10 ? "0" : "") + String(hour, DEC) + "-";
+    currentDate += (minute < 10 ? "0" : "") + String(minute, DEC);
   }
 }
 
@@ -234,12 +252,8 @@ byte calculateLocalChecksum() {
 
 String generateHtmlPage() {
   String html = "<!DOCTYPE HTML>";
-  html += "<html><head><meta charset=\"UTF-8\"><style>";
-  html += "h1 { color: #333; margin-bottom: 20px; }";
-  html += "p { font-size: 18px; }";
-  html += ".sensor-data { margin-bottom: 30px; }";
-  html += "button { margin-top: 10px; }";
-  html += "canvas { max-width: 100%; height: 300px; margin-top: 20px; }";
+  html += "<html><head><style>";
+  html += "canvas{max-width: 100%; height: 300px;margin-top:20px;}";
   html += "</style>";
   html += "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>";
   html += "<script src='https://cdn.jsdelivr.net/npm/chart.js'></script>";
@@ -248,19 +262,17 @@ String generateHtmlPage() {
   html += "<h1 class='text-center'>Sensor Data</h1>";
   
   // Sensor Data Display Section
-  html += "<div class='sensor-data'>";
-  html += "<div class='row'>";
-  html += "  <div class='col-md-6'><p><strong>Date:</strong> <span id='date'>" + dateString + "</span></p></div>";
-  html += "  <div class='col-md-6'><p><strong>Light:</strong> <span id='light'>" + lightString + "</span> Lux</p></div>";
+  html += "<div class='row text-center'>";
+  html += "  <div class='col-md-6'><p><strong>Date:</strong><span id='date'> " + dateString + "</span></p></div>";
+  html += "  <div class='col-md-6'><p><strong>Light:</strong><span id='light'> " + lightString + "</span> Lux</p></div>";
   html += "</div>";
-  html += "<div class='row'>";
-  html += "  <div class='col-md-6'><p><strong>Humidity:</strong> <span id='humidity'>" + humidityString + "</span> %</p></div>";
-  html += "  <div class='col-md-6'><p><strong>Temperature:</strong> <span id='temperature'>" + temperatureString + "</span> °C</p></div>";
+  html += "<div class='row text-center'>";
+  html += "  <div class='col-md-6'><p><strong>Humidity:</strong><span id='humidity'> " + humidityString + "</span> %</p></div>";
+  html += "  <div class='col-md-6'><p><strong>Temperature:</strong><span id='temperature'> " + temperatureString + "</span> °C</p></div>";
   html += "</div>";
-  html += "<div class='row'>";
-  html += "  <div class='col-md-6'><p><strong>Button State:</strong> <span id='button'>" + buttonString + "</span></p></div>";
-  html += "  <div class='col-md-6'><p><strong>Motor Running:</strong> <span id='motor'>" + motorRunningString + "</span></p></div>";
-  html += "</div>";
+  html += "<div class='row text-center'>";
+  html += "  <div class='col-md-6'><p><strong>Button State:</strong><span id='button'> " + buttonString + "</span></p></div>";
+  html += "  <div class='col-md-6'><p><strong>Motor Running:</strong><span id='motor'> " + motorRunningString + "</span></p></div>";
   html += "</div>";
 
   // Motor Control Section
@@ -272,87 +284,90 @@ String generateHtmlPage() {
 
   // Chart Section
   html += "<div class='card'>";
-  html += "  <div class='card-body'>";
-  html += "    <h5 class='card-title'>Sensor Data Over Time</h5>";
-  html += "    <canvas id='sensorGraph'></canvas>";
-  html += "  </div>";
+  html += "<div class='card-body'>";
+  html += " <h5 class='card-title'>Sensor Data Over Time</h5>";
+  html += " <canvas id='sensorGraph'></canvas>";
+  html += "</div>";
   html += "</div>";
 
+  // older data
   html += "<div class='mt-5 d-flex flex-column align-items-center justify-content-center'>";
-  html += "<h3>Retrieve Older Data</h3>";
-  html += "<label for='timePicker'>Select Time:</label>";
-  html += "<input type='datetime-local' id='timePicker' class='form-control w-25'>";
-  html += "<button class='btn btn-primary mt-2' onclick='fetchOlderData()'>Fetch Data</button>";
+  html += " <h3>Retrieve Older Data</h3>";
+  html += " <label for='timePicker'>Select Time:</label>";
+  html += " <input type='datetime-local' id='timePicker' class='form-control w-25'>";
+  html += " <button class='btn btn-primary mt-2' onclick='fetchOlderData()'>Fetch Data</button>";
+  html += "<div class='mt-3' id='output'></div>";
   html += "</div>";
 
-  // JavaScript for Chart and Sensor Data Update
+  html += "</div>";
+
   html += "<script>";
+  // JavaScript for Chart and Sensor Data Update
   html += "function startMotor() {";
-  html += "  var duration = document.getElementById('duration').value;";
-  html += "  fetch('/startMotor?duration=' + duration, { method: 'GET' })";
-  html += "    .then(response => response.json())";
-  html += "    .then(data => {";
-  html += "      console.log(data);";
-  html += "    }).catch(error => console.error('Error starting motor:', error));";
+  html += "fetch('/startMotor?duration=' + document.getElementById('duration').value)";
+  html += ".catch(error => {});";
   html += "}";
 
   html += "function updateSensorData() {";
-  html += "  fetch('/getData').then(response => response.json()).then(data => {";
-  html += "    document.getElementById('date').innerText = data.date;";
-  html += "    document.getElementById('light').innerText = data.light;";
-  html += "    document.getElementById('humidity').innerText = data.humidity;";
-  html += "    document.getElementById('temperature').innerText = data.temperature;";
-  html += "    document.getElementById('button').innerText = data.button;";
-  html += "    document.getElementById('motor').innerText = data.motor;";
+  html += " fetch('/getData').then(response => response.json()).then(data => {";
+  html += "document.getElementById('date').innerText = data.date;";
+  html += "document.getElementById('light').innerText = data.light;";
+  html += "document.getElementById('humidity').innerText = data.humidity;";
+  html += "document.getElementById('temperature').innerText = data.temperature;";
+  html += "document.getElementById('button').innerText = data.button;";
+  html += "document.getElementById('motor').innerText = data.motor;";
     // Add the new data to the graph";
-  html += "    const time = data.date;";
-  html += "    if (!sensorGraph.data.labels.includes(time)) {";
-  html += "      sensorGraph.data.labels.push(time);";
-  html += "      sensorGraph.data.datasets[0].data.push(data.temperature);";
-  html += "      sensorGraph.data.datasets[1].data.push(data.humidity);";
-  html += "      sensorGraph.data.datasets[2].data.push(data.light);";
-  html += "      sensorGraph.update();";
-  html += "    }";
-  html += "  }).catch(error => console.error('Error fetching data:', error));";
+  html += "const time = data.date;";
+  html += "if (!sensorGraph.data.labels.includes(time)) {";
+  html += "sensorGraph.data.labels.push(time);";
+  html += "sensorGraph.data.datasets[0].data.push(data.temperature);";
+  html += "sensorGraph.data.datasets[1].data.push(data.humidity);";
+  html += "sensorGraph.data.datasets[2].data.push(data.light);";
+  html += "sensorGraph.update();";
+  html += " }";
+  html += "}).catch(error => {});";
   html += "}";
 
     // Initialize the Chart.js graph
   html += "const ctx = document.getElementById('sensorGraph').getContext('2d');";
   html += "const sensorGraph = new Chart(ctx, {";
-  html += "  type: 'line',";
-  html += "  data: {";
-  html += "    labels: [],"; // Timestamps
-  html += "    datasets: [";
-  html += "      { label: 'Temperature (°C)', data: [], borderColor: 'red', fill: false },";
-  html += "      { label: 'Humidity (%)', data: [], borderColor: 'blue', fill: false },";
-  html += "      { label: 'Light (Lux)', data: [], borderColor: 'yellow', fill: false }";
-  html += "    ]";
-  html += "  },";
-  html += "  options: {";
-  html += "    responsive: true,";
-  html += "    plugins: { legend: { position: 'top' } },";
-  html += "    scales: { x: { title: { display: true, text: 'Time' } }, y: { title: { display: true, text: 'Values' } } }";
-  html += "  }";
+  html += "type: 'line',";
+  html += "data: {";
+  html += "labels: [],"; // Timestamps
+  html += "datasets: [";
+  html += "{ label: 'Temperature (°C)', data:[], borderColor: 'red', fill: false },";
+  html += "{ label: 'Humidity (%)',data:[], borderColor: 'blue', fill: false },";
+  html += "{ label: 'Light (Lux)',data:[], borderColor: 'yellow', fill: false }";
+  html += "]";
+  html += " },";
+  html += "options: {";
+  html += "responsive: true,";
+  html += "plugins: { legend: { position: 'top' } },";
+  html += "scales: { x: { title: { display: true, text: 'Time' }}, y:{ title: { display: true, text: 'Values' } } }";
+  html += "}";
   html += "});";
 
   html += "function fetchOlderData() {";
   html += "  var time = document.getElementById('timePicker').value;";
   html += "  if (time) {";
-  html += "    var formattedTime = time.replace('T', ' ').substring(0, 19);";
-  html += "    fetch('/getOlderData?time=' + formattedTime, { method: 'GET' })";
-  html += "      .then(response => response.json())";
-  html += "      .then(data => {";
-  html += "        console.log(data);";
-  html += "      }).catch(error => console.error('Error fetching data:', error));";
-  html += "  } else {";
-  html += "    alert('Please select a valid time.');";
-  html += "  }";
+  html += "var date = new Date(time);";
+  html += "var formattedDate = date.getFullYear() + '-';";
+  html += "formattedDate += (date.getMonth() + 1 < 10 ? '0' : '')+(date.getMonth() + 1)+'-'";
+  html += "+(date.getDate() < 10 ? '0' : '') + date.getDate() + '-'";
+  html += "+(date.getHours() < 10 ? '0' : '') + date.getHours() + '-'";
+  html += "+(date.getMinutes() < 10 ? '0' : '') + date.getMinutes();";
+  html += "fetch('/requestOlderData?time=' + formattedDate);";
+  html += "setTimeout(() => fetch('/getOlderData?time=' + formattedDate).then(r => r.json()).then(data=>{if(data.error){output.innerHTML='<p>Error:' + data.error + '</p>';}";
+  html += "else {output.innerHTML='<p><strong>Humidity: '+data.data.humidity+'%</strong></p>' + '<p><strong>Light: '+data.data.light+'</strong></p>' + '<p><strong>Temperature: ' + data.data.temperature + '°C</strong></p>';}}), 1000);";
+  html += "} else {";
+  html += "alert('Please select a valid time.');";
+  html += "}";
   html += "}";
 
   html += "setInterval(updateSensorData, 900);";
   html += "updateSensorData();";
   html += "</script>";
-  html += "</div></body></html>";
+  html += "</body></html>";
   return html;
 }
 
@@ -371,41 +386,45 @@ void saveDataToFirebase() {
   }
 }
 
-// String getPaginatedFirebaseData(String date, int limit) {
-//   String path = "/sensorData/" + date;
-//   QueryFilter query;
-//   query.orderBy("$key");
-//   query.limitToLast(limit);
+String getFirebaseData(String date) {
+  String path = "/sensorData/" + date;
+  QueryFilter query;
+  query.orderBy("$key");
+  query.limitToLast(1);
 
-//   if (Firebase.RTDB.getJSON(&fbdo, path, &query)) {
-//     if (fbdo.dataType() == "json") {
-//       FirebaseJson &json = fbdo.jsonObject();
-//       size_t dataCount = json.iteratorBegin();
-//       String jsonResponse = "{";
-//       jsonResponse += "\"data\": [";
-//       for (size_t i = 0; i < dataCount; i++) {
-//         String key, value;
-//         int type;
-//         json.iteratorGet(i, type, key, value);
+  if (Firebase.RTDB.getJSON(&fbdo, path, &query)) {
+    if (fbdo.dataType() == "json") {
+      FirebaseJson &json = fbdo.jsonObject();
+      size_t dataCount = json.iteratorBegin();
+      String jsonResponse = "{";
+      jsonResponse += "\"data\": ";
+      for (size_t i = 0; i < dataCount; i++) {
+        String key, value;
+        int type;
+        json.iteratorGet(i, type, key, value);
 
-//         if (value.startsWith("{")) {
-//           if (i > 0) {
-//             jsonResponse += ",";
-//           }
-//           jsonResponse += value;
-//         }
-//       }
-//       json.iteratorEnd();
-//       jsonResponse += "]";
-//       jsonResponse += "}";
-//       return jsonResponse;
-//     } else {
-//       Serial.println("No sensor data found for the specified date.");
-//       return "{\"error\": \"No sensor data found\"}";
-//     }
-//   } else {
-//     Serial.println("Error fetching data: " + fbdo.errorReason());
-//       return "{\"error\": \"" + fbdo.errorReason() + "\"}";
-//   }
-// }
+        if (value.startsWith("{")) {
+          jsonResponse += value;
+        }
+      }
+      json.iteratorEnd();
+      jsonResponse += "}";
+      return jsonResponse;
+    } else {
+      Serial.println("No sensor data found for the specified date.");
+      return "{\"error\": \"No sensor data found\"}";
+    }
+  } else {
+    Serial.println("Error fetching data: " + fbdo.errorReason());
+      return "{\"error\": \"" + fbdo.errorReason() + "\"}";
+  }
+}
 
+void processTimeParamArray() {
+  for (int i = 0; i < timeParamCount; i++) {
+    String firebaseData = getFirebaseData(timeParamArray[i]);
+    olderDataDictionary[timeParamArray[i]] = firebaseData;
+    timeParamArray[i] = "";
+  }
+  timeParamCount = 0;
+}
